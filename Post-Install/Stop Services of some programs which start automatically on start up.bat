7a37435fe7dc9a6feabba49922992c952a4edf99
Sharing is Caring <prabeshrajjk@gmail.com>
2021-04-05 16:45:15 +0545

    Update and rename Stop Services.bat to Stop Services of some programs which start automatically on start up.bat

--- Post-Install/Stop Services of some programs which start automatically on start up.bat
+++ Post-Install/Stop Services of some programs which start automatically on start up.bat
@@ -1,3 +1,5 @@
+:: TO list currently running services on startup
+::
 :: Open administrative command prompt and type "WMIC" then enter.
 ::  Type "startup" for a list of programs.
 
@@ -17,3 +19,12 @@ sc config IDMan start= disabled
 
 EXIT
 :: cmd /k  
+
+:: Regisrty location  
+:: HKU\S-1-5-19\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+:: HKU\S-1-5-20\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+:: HKU\S-1-5-21-2988739662-2026086280-828728085-1001\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+:: OneDrive
+:: Wechat
+:: IDMan
+:: Viber
