7fda34ddaf0f914a0b5c033a4c1a16d23e7b05dc
Sharing is Caring <prabeshrajjk@gmail.com>
2021-04-07 16:55:03 +0545

    Rename Pre Install/Export all saved wifi passwords from this computer.ps1 to Pre Install/WiFi/Export all saved wifi passwords from this computer.ps1

--- Pre Install/Export all saved wifi passwords from this computer.ps1
+++
@@ -1,7 +0,0 @@
-<#
-   This Powershell script works when you manually open the powershell with admin prev and copy paste the following code 
-change to $env:c:\  to  $env:PSScriptRoot  if the code runs from script 
-#>
-
-
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "$env:C:\WirelessNetworkPasswords.txt"
