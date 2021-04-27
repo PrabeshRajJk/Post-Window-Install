67fed01810dca47d65af5f7f58b912f8d741e07e
Sharing is Caring <prabeshrajjk@gmail.com>
2021-04-27 14:44:44 +0545

    Update Export all saved wifi passwords from this computer in one file.ps1

--- Pre Install/WiFi/Export all saved wifi passwords from this computer in one file.ps1
+++ Pre Install/WiFi/Export all saved wifi passwords from this computer in one file.ps1
@@ -1,7 +1,9 @@
 <#
    This Powershell script works when you manually open the powershell with admin prev and copy paste the following code 
 change to $env:c:\  to  $env:PSScriptRoot  if the code runs from script 
+$DesktopPath if
+$env:$ScriptPath
 #>
 
 
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "$env:$ScriptPath\WirelessNetworkPasswords.txt"
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "$env:C:\WirelessNetworkPasswords.txt"
