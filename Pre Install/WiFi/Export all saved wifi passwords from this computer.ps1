7908c07f5944a10865e476a3a5b8fd3f852924a0
Sharing is Caring <prabeshrajjk@gmail.com>
2021-04-27 14:20:56 +0545

    Update Export all saved wifi passwords from this computer.ps1

--- Pre Install/WiFi/Export all saved wifi passwords from this computer.ps1
+++ Pre Install/WiFi/Export all saved wifi passwords from this computer.ps1
@@ -4,4 +4,4 @@ change to $env:c:\  to  $env:PSScriptRoot  if the code runs from script
 #>
 
 
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "$env:C:\WirelessNetworkPasswords.txt"
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "$env:$ScriptPath\WirelessNetworkPasswords.txt"
